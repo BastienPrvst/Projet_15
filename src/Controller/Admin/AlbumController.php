8611e84d8acd868aa585957550ed61c82e1df25f
Bastien <a@a.a>
2025-08-11 18:30:02 +0200

    Passage projet en Symfony 7.2 + passage des routes en Attribut

--- src/Controller/Admin/AlbumController.php
+++ src/Controller/Admin/AlbumController.php
@@ -3,30 +3,28 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Album;
-use App\Entity\Media;
 use App\Form\AlbumType;
-use App\Form\MediaType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
 
 class AlbumController extends AbstractController
 {
-    /**
-     * @Route("/admin/album", name="admin_album_index")
-     */
-    public function index()
-    {
+
+	#[Route(path: '/admin/album', name: 'admin_album_index')]
+	public function index(): Response
+	{
         $albums = $this->getDoctrine()->getRepository(Album::class)->findAll();
 
         return $this->render('admin/album/index.html.twig', ['albums' => $albums]);
     }
 
-    /**
-     * @Route("/admin/album/add", name="admin_album_add")
-     */
-    public function add(Request $request)
-    {
+
+	#[Route(path: '/admin/album/add', name: 'admin_album_add')]
+	public function add(Request $request): RedirectResponse|Response
+	{
         $album = new Album();
         $form = $this->createForm(AlbumType::class, $album);
         $form->handleRequest($request);
@@ -41,11 +39,10 @@ class AlbumController extends AbstractController
         return $this->render('admin/album/add.html.twig', ['form' => $form->createView()]);
     }
 
-    /**
-     * @Route("/admin/album/update/{id}", name="admin_album_update")
-     */
-    public function update(Request $request, int $id)
-    {
+
+	#[Route(path: '/admin/album/update/{id}', name: 'admin_album_update')]
+	public function update(Request $request, int $id): RedirectResponse|Response
+	{
         $album = $this->getDoctrine()->getRepository(Album::class)->find($id);
         $form = $this->createForm(AlbumType::class, $album);
         $form->handleRequest($request);
@@ -59,11 +56,10 @@ class AlbumController extends AbstractController
         return $this->render('admin/album/update.html.twig', ['form' => $form->createView()]);
     }
 
-    /**
-     * @Route("/admin/album/delete/{id}", name="admin_album_delete")
-     */
-    public function delete(int $id)
-    {
+
+	#[Route(path: '/admin/album/delete/{id}', name: 'admin_album_delete')]
+	public function delete(int $id): RedirectResponse
+	{
         $media = $this->getDoctrine()->getRepository(Album::class)->find($id);
         $this->getDoctrine()->getManager()->remove($media);
         $this->getDoctrine()->getManager()->flush();
--- src/Controller/Admin/MediaController.php
+++ src/Controller/Admin/MediaController.php
@@ -5,16 +5,17 @@ namespace App\Controller\Admin;
 use App\Entity\Media;
 use App\Form\MediaType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
 
 class MediaController extends AbstractController
 {
-    /**
-     * @Route("/admin/media", name="admin_media_index")
-     */
-    public function index(Request $request)
-    {
+	#[Route(path: '/admin/media', name: 'admin_media_index')]
+	public function index(Request $request): Response
+	{
         $page = $request->query->getInt('page', 1);
 
         $criteria = [];
@@ -38,11 +39,9 @@ class MediaController extends AbstractController
         ]);
     }
 
-    /**
-     * @Route("/admin/media/add", name="admin_media_add")
-     */
-    public function add(Request $request)
-    {
+	#[Route(path: '/admin/media/add', name: 'admin_media_add')]
+	public function add(Request $request): RedirectResponse|Response
+	{
         $media = new Media();
         $form = $this->createForm(MediaType::class, $media, ['is_admin' => $this->isGranted('ROLE_ADMIN')]);
         $form->handleRequest($request);
@@ -62,11 +61,10 @@ class MediaController extends AbstractController
         return $this->render('admin/media/add.html.twig', ['form' => $form->createView()]);
     }
 
-    /**
-     * @Route("/admin/media/delete/{id}", name="admin_media_delete")
-     */
-    public function delete(int $id)
-    {
+
+	#[Route(path: '/admin/media/delete/{id}', name: 'admin_media_delete')]
+	public function delete(int $id): RedirectResponse
+	{
         $media = $this->getDoctrine()->getRepository(Media::class)->find($id);
         $this->getDoctrine()->getManager()->remove($media);
         $this->getDoctrine()->getManager()->flush();
